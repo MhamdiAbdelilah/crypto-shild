1bbca4584fe78f4493f8e2e45b77d71b0676dc35
bilal.taoufik@lycee-jeanrostand.fr <bilal.taoufik@lycee-jeanrostand.fr>
2024-05-07 12:31:53 +0200

    2

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -6,7 +6,7 @@ def open_file():
     file = filedialog.askopenfilename()
     if file:
         # Affiche le nom du fichier sélectionné dans le label
-        file_label.config(text=f"Fichier sélectionné : {file}")
+        file_label.config(text= f"Fichier sélectionné : {file}")
 
 def crypter():
     # Ajoutez ici le code pour crypter
@@ -38,13 +38,17 @@ tab_control = ttk.Notebook(root)
 tab1 = tk.Frame(tab_control)
 tab2 = tk.Frame(tab_control)
 
-# Bouton d'importation de fichier
-btn_import = tk.Button(root, text="Sélectionner le fichier", command=open_file)
-btn_import.place(relx=0.5, rely=0.1, anchor='center')  # Place le bouton en haut
+text_label = tk.Label(root, text="Sélectionner un document : ", relief="flat", padx=10, pady=5)
+text_label.place(x=1, y=35., anchor="nw")
 
 # Label pour afficher le nom du fichier sélectionné
-file_label = tk.Label(root, text="Aucun fichier sélectionné")
-file_label.place(relx=0.5, rely=0.15, anchor='center')  # Place le label en dessous du bouton
+file_label = tk.Label(root, text="Aucun fichier sélectionné", borderwidth=2, relief="sunken", padx=10, pady=5)
+file_label.place(x=10, y=70, anchor='nw')  # Place le label en dessous du bouton
+
+# Bouton d'importation de fichier
+btn_import = tk.Button(root, text="...", command=open_file)
+btn_import.config(width=4, height=1, bd=2)
+btn_import.place(relx=0.9, rely=0.124, anchor='center')  # Place le bouton en haut
 
 tab_control.add(tab1, text='Crypter')
 tab_control.add(tab2, text='Décrypter')
