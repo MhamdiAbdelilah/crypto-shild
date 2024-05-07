a035cb98c61142a7bb9ff7420b6d0361f8eb746a
MHAMDI ABDELILAH <mhamdi_a@JEAN-ROSTAND.lan>
2024-05-07 12:30:19 +0200

    creat the shap of algorithme to follow

--- algothme.py
+++ algothme.py
@@ -1,7 +1,6 @@
-
 import tkinter as tk
 from tkinter import filedialog
-
+import numpy as np
 
 def import_file(path):
     with open(path, 'rb') as file:  
@@ -45,9 +44,41 @@ def get_path():
     file_path = filedialog.askopenfilename()
     root.destroy()
     return file_path
+def split_by_n(seq, n):
+    """A generator to divide a sequence into chunks of n units."""
+    while seq:
+        yield seq[:n]
+        seq = seq[n:]
+
+def to_matrix44(file:np.array):
+    list=np.reshape(file,4,4)
+    return list
 
 
-path: str=get_path()
-file: str =import_file(path)+b'encrepted'
-export_file(remove_tag(path),file)
-print(file)
+path:str =get_path()
+file:str =import_file(path)
+file_list: np.array=list(split_by_n(file, 16))
+#import the key here
+
+
+xfile= np.array([]) # declar the arry where we save the result of encreption or decreption 
+for i in range(len(file_list)):
+    list1: list=list(split_by_n(file_list[i],1))
+    list1=np.array(list1)
+    try:
+        list1= np.reshape(list1,(4,4))
+    except:
+        list1=np.array(list1)
+    #the round shoud start from here
+
+
+    #to her
+
+
+    #save the list in xfile 
+    xfile=np.append(xfile,list1)
+
+# the last round here
+
+
+print(xfile)
