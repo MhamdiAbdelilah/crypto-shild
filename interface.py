4cd0fa95e233fd0f556fcc937bec599178809bc9
MHAMDI ABDELILAH <mhamdi_a@JEAN-ROSTAND.lan>
2024-05-13 12:27:44 +0200

    changen the interface

--- interface.py
+++ interface.py
@@ -14,7 +14,7 @@ def open_file() -> None:
 
 
 def import_key() -> None:
-    global key
+    global key, key_label
     key_file = filedialog.askopenfilename(filetypes=[("Key Files", "*.key")])
     if key_file:
         key_label.config(text=f"Fichier sélectionné : {key_file}")
@@ -24,9 +24,12 @@ def import_key() -> None:
 
 
 def save_key() -> None:
+    global option
     n = option.get() // 8
     gen_key = al.generate_key(n)
-    al.export_file(al.get_path('key file ', '.key'), gen_key)
+    key_path = al.get_path('key file ', '.key')
+    al.export_file(key_path , gen_key)
+    save_file.config(text=f"Fichier sélectionné : {key_path}")
     print("Clé enregistrée.")
 
 
@@ -48,22 +51,17 @@ def confirm() -> None:
 
         print("Opération confirmée.")
 
-
 def onglet_crypter(tab):
+    global file_label, text3_label, option, key_label, save_file
+
     text1_label = tk.Label(tab, text="Sélectionner un document à crypter : ", relief="flat", padx=10, pady=5)
     text1_label.place(x=1, y=35)
-
-    def choisir_file():
-        file = filedialog.askopenfilename()
-        if file:
-            file_label.config(text=f"Fichier sélectionné : {file}")
-
 
     file_label = tk.Label(tab, text="Aucun fichier sélectionné", borderwidth=2, relief="sunken", padx=10, pady=5)
     file_label.place(x=10, y=70)
 
     # Bouton d'importation de fichier
-    btn_import = tk.Button(tab, text="...", command=choisir_file)
+    btn_import = tk.Button(tab, text="...", command=open_file)
     btn_import.config(width=4, height=1, bd=2, cursor="hand2", overrelief="solid")
     btn_import.place(relx=0.86, rely=0.132)
 
@@ -87,14 +85,9 @@ def onglet_crypter(tab):
     check3 = tk.Radiobutton(tab, text="256", variable=option, value=256)
     check3.place(x=400, y=200)
 
-    def save_file():
-        file = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
-        if file:
-            save_file.config(text=f"Fichier enregistré : {file}")
-            print(f"Fichier enregistré : {file}")
 
     # Bouton de sauvegarde de fichier
-    btn_save = tk.Button(tab, text="Enregistrer", command=save_file)
+    btn_save = tk.Button(tab, text="Enregistrer", command=save_key)
     btn_save.config(width=10, height=1, bd=2, cursor="hand2", overrelief="solid")
     btn_save.place(x=450, y=255)
 
@@ -107,52 +100,34 @@ def onglet_crypter(tab):
     text4_label = tk.Label(tab, text="Importer sa clé : ", relief="flat", padx=10, pady=5)
     text4_label.place(x=1, y=320)
 
-    def cle_file():
-        file = filedialog.askopenfilename()
-        if file:
-            cle_label.config(text=f"Clé sélectionnée : {file}")
 
 
-    cle_label = tk.Label(tab, text="Aucune clé sélectionnée", borderwidth=2, relief="sunken", padx=10, pady=5)
-    cle_label.place(x=10, y=360)
+    key_label = tk.Label(tab, text="Aucune clé sélectionnée", borderwidth=2, relief="sunken", padx=10, pady=5)
+    key_label.place(x=10, y=360)
 
-    cle_import = tk.Button(tab, text="...", command=cle_file)
+    cle_import = tk.Button(tab, text="...", command=import_key)
     cle_import.config(width=4, height=1, bd=2, cursor="hand2", overrelief="solid")
     cle_import.place(relx=0.86, rely=0.668)
 
     # Bouton confirmer
-    btn_confirmer = tk.Button(tab, text="Confirmer", command=None)
+    btn_confirmer = tk.Button(tab, text="Confirmer", command=confirm)
     btn_confirmer.config(width=10, height=1, bd=2, cursor="hand2", overrelief="solid")
     btn_confirmer.place(x=250, y=480)
 
 
-
-
-
-
-
-
-
-
-
-
-
 # l'onglet "Décrypter"
 def onglet_decrypter(tab):
+    global file_label, text3_label, option, key_label, save_file
     text1_label = tk.Label(tab, text="Sélectionner un document à décrypter : ", relief="flat", padx=10, pady=5)
     text1_label.place(x=1, y=35)
 
-    def choisir_file():
-        file = filedialog.askopenfilename()
-        if file:
-            file_label.config(text=f"Fichier sélectionné : {file}")
 
 
     file_label = tk.Label(tab, text="Aucun fichier sélectionné", borderwidth=2, relief="sunken", padx=10, pady=5)
     file_label.place(x=10, y=70)
 
 
-    btn_import = tk.Button(tab, text="...", command=choisir_file)
+    btn_import = tk.Button(tab, text="...", command=open_file)
     btn_import.config(width=4, height=1, bd=2, cursor="hand2", overrelief="solid")
     btn_import.place(relx=0.86, rely=0.132)
 
@@ -166,25 +141,17 @@ def onglet_decrypter(tab):
     option = tk.StringVar(value="none")
 
   
-    def select_option():
-        print(f"Option sélectionnée : {option.get()}")
 
-    check1 = tk.Radiobutton(tab, text="128", variable=option, value="128", command=select_option)
+    check1 = tk.Radiobutton(tab, text="128", variable=option, value="128",)
     check1.place(x=100, y=200)
-    check2 = tk.Radiobutton(tab, text="192", variable=option, value="192", command=select_option)
+    check2 = tk.Radiobutton(tab, text="192", variable=option, value="192",)
     check2.place(x=250, y=200)
-    check3 = tk.Radiobutton(tab, text="256", variable=option, value="256", command=select_option)
+    check3 = tk.Radiobutton(tab, text="256", variable=option, value="256",)
     check3.place(x=400, y=200)
 
 
-    def save_file():
-        file = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
-        if file:
-            # ajouter le code pour enregistrer les données dans le fichier sélectionné
-            print(f"Le fichier est enregistré dans : {file}")
-
     # Bouton de sauvegarde de fichier
-    btn_save = tk.Button(tab, text="Enregistrer", command=save_file)
+    btn_save = tk.Button(tab, text="Enregistrer" )
     btn_save.config(width=10, height=1, bd=2)
     btn_save.place(x=450, y=250)
 
@@ -194,41 +161,26 @@ def onglet_decrypter(tab):
     text4_label = tk.Label(tab, text="Importer sa clé : ", relief="flat", padx=10, pady=5)
     text4_label.place(x=1, y=320)
 
-    
-
-    def cle():
-        file = filedialog.askopenfilename()
-        if file:
-            cle_label.config(text=f"Clé sélectionnée : {file}")
 
 
-    cle_label = tk.Label(tab, text="Aucune clé sélectionnée", borderwidth=2, relief="sunken", padx=10, pady=5)
-    cle_label.place(x=10, y=360)
 
-    cle_import = tk.Button(tab, text="...", command=cle)
+    key_label = tk.Label(tab, text="Aucune clé sélectionnée", borderwidth=2, relief="sunken", padx=10, pady=5)
+    key_label.place(x=10, y=360)
+
+    cle_import = tk.Button(tab, text="...", command=import_key)
     cle_import.config(width=4, height=1, bd=2, cursor="hand2", overrelief="solid")
     cle_import.place(relx=0.86, rely=0.668)
 
-    btn_confirmer = tk.Button(tab, text="Confirmer", command=None)
+    btn_confirmer = tk.Button(tab, text="Confirmer", command=confirm)
     btn_confirmer.config(width=10, height=1, bd=2, cursor="hand2", overrelief="solid")
     btn_confirmer.place(x=250, y=480)
 
 
     # Griser les options
-    def desactiver_options():
-        check1.config(state="disabled")
-        check2.config(state="disabled")
-        check3.config(state="disabled")
-        btn_save.config(state="disabled")
-
-
-    desactiver_options()
-
-
-
-
-
-
+    check1.config(state="disabled")
+    check2.config(state="disabled")
+    check3.config(state="disabled")
+    btn_save.config(state="disabled")
 
 
 
