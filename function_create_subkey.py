3f45b60f78e37f768c6b7066a152f151f5a519a3
SANGARALINGAM LAKSHAN <lakshan.sangaralingam@lycee-jeanrostand.fr>
2024-05-16 09:10:57 +0200

    function create_subkey

--- function_create_subkey.py
+++ function_create_subkey.py
@@ -8,5 +8,5 @@ def create_subkey(main_key, identifier: str, nbr: int) -> bytes:
     combined = main_key + identifier.encode()
     subkey = hashlib.sha256(combined).digest()[:nbr]
     return subkey
-print(create_subkey)
 
+print(create_subkey(b'\xc3\xce\xcad\x84\x82\x04\x90\xb5\xbf\x87\xc7\xbcZ\x99\x04','lakshan',16)) 
